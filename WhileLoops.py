eba1ad6f07a2ea39557bc7e039d00176cfd9edc0
Jayden Asante <jja33@kent.ac.uk>
2024-10-23 01:27:16 +0100

    completed

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,33 @@
 ### Part Two -- your code goes here. 
+import math
+import random
+
+def main():
+    rn = random.randint(1, 100)
+    no = int(input("Guess the number: "))                             
+    rep(rn, no)
+    
+    
+    
+def rep(rn, no):
+    while rn != no:
+        
+        if no < rn:
+            print("Too low!")
+        else:
+            print("Too high!")
+        
+    
+        x = input("This is not the right number. Want to guess again? (yes/no): ").strip().lower()
+        
+        if x == "yes":
+            no = int(input("Guess the number: "))
+        elif x == "no":
+            print("The number was", rn)
+            print("Quiting Application...")
+            break
+    else:
+        print("Correct. The number was", rn)
+        print("Quiting Application...")
+           
+main()
--- forLoop.py
+++ forLoop.py
@@ -1,1 +1,9 @@
 ### Part One -- your code goes here. 
+import math     #This imports the maths module into the program
+
+def main():     #Defines the function main
+    base = list(range(1, 11, 1))        #This makes a list from 1 to 10 using "range()", and then puts it in a list using "list()". It is then labled "base".
+    for x in base:      #This is a for loop which assigns "x" to be each value in the "base" function.
+        print("The square of", x, "is", pow(x, 2))      #This prints the square of x - all the values in the list.
+
+main()
